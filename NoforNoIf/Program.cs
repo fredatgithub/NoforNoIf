8437463bfa62bda66b4fb23a6da48c2f6ab74b8e
Freddy Juhel <domaine75@gmail.com>
2017-09-12 21:32:54 +0200

    Initial commit

---
+++ NoforNoIf/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace NoforNoIf
+{
+  class Program
+  {
+    public int i = 1;
+    static void Main(string[] args)
+    {
+      sixtyFour();
+      thirtyTwo();
+      four();
+      Console.ReadKey();
+    }
+
+    public static void one() { Console.WriteLine(i++); }
+    public void two() { one(); one(); }
+    public void four() { two(); two(); }
+    public void eight() { four(); four(); }
+    public void sixteen() { eight(); eight(); }
+    public void thirtyTwo() { sixteen(); sixteen(); }
+    public void sixtyFour() { thirtyTwo(); thirtyTwo(); }
+  }
+}
